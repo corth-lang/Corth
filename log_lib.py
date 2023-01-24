c4589aff97f145ae34557dd5939298c522465f46
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2023-01-24 15:48:17 +0300

    Modified command function, now it shows the returncode if not zero

--- log_lib.py
+++ log_lib.py
@@ -9,5 +9,20 @@ def log(type_, message):
         
 def command(command):
     log("CMD", command)
-    subprocess.run(command)
+
+    try:
+        process = subprocess.run(command)
+
+    except KeyboardInterrupt:
+        log("INFO", "Interrupted while executing command")
+
+    except Exception as e:
+        log("ERROR", f"Unhandled error; {type(e)} '{e}'")
+
+    else:
+        if process.returncode:
+            log("ERROR", f"Command returned non-zero return code; got {process.returncode}")
+
+        else:
+            log("INFO", "Successfully executed command")
 
