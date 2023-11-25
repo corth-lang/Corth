6d4b0213f8fcf959062516e9636e4ea67793a7da
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2023-11-25 22:50:37 +0300

    Updated bootstrap.sh and removed build directory
    - corth executable is built in Corth/build while bootstrapping and then moved to Corth directory.
    - Corth/build directory is now removed after bootstrapping.

--- Corth/bootstrap.sh
+++ Corth/bootstrap.sh
@@ -1,34 +1,50 @@
-./Corth/build/corth compile-nasm ./Corth/compiler/corth.corth ./Corth/build/corth.asm
+mkdir ./Corth/build/
+
+./Corth/corth compile-nasm ./Corth/compiler/corth.corth ./Corth/build/corth.asm
 compile_exit=$?
 
 if [[ $compile_exit != 0 ]]; then
-   echo "[build.sh] [ERROR] Could not compile to NASM."
-   rm ./Corth/build/corth.asm
+   echo '[build.sh] [ERROR] Could not compile to NASM.'
+   rm -rf ./Corth/build/
    exit $compile_exit
 fi
 
-echo "[build.sh] [INFO] Compiled to NASM."
+echo '[build.sh] [INFO] Compiled to NASM.'
 
 nasm ./Corth/build/corth.asm -f elf64 -o ./Corth/build/corth.o
 nasm_exit=$?
-rm ./Corth/build/corth.asm
 
 if [[ $nasm_exit != 0 ]]; then
-   echo "[build.sh] [ERROR] Could not compile NASM program."
-   rm ./Corth/build/corth.o
+   echo '[build.sh] [ERROR] Could not compile NASM program.'
+   rm -rf ./Corth/build/
    exit $nasm_exit
 fi
 
-echo "[build.sh] [INFO] Created the object file."
+echo '[build.sh] [INFO] Created the object file.'
 
 ld ./Corth/build/corth.o -o ./Corth/build/corth
 linker_exit=$?
-rm ./Corth/build/corth.o
 
 if [[ $linker_exit != 0 ]]; then
-   echo "[build.sh] [ERROR] Could not link program."
-   rm ./Corth/build/corth
+   echo '[build.sh] [ERROR] Could not link program.'
+   rm -rf ./Corth/build/
    exit $linker_exit
 fi
 
-echo "[build.sh] [INFO] Done."
+echo '[build.sh] [INFO] Starting tests...'
+
+./PythonCompiler/main.py test -ceo
+tests_exit=$?
+
+if [[ $tests_exit != 0 ]]; then
+    echo '[build.sh] [ERROR] Tests failed.'
+    rm -rf ./Corth/build/
+    exit $tests_exit
+fi
+
+echo '[build.sh] [INFO] Tests succeded, moving ./Corth/build/corth to ./Corth/corth.'
+
+mv ./Corth/build/corth ./Corth/corth
+rm -rf ./Corth/build/
+
+echo '[build.sh] [INFO] Done.'
